67d6e0073330e878693d720fbe2d16a71c19c2b3
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 19:19:30 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan13/relational/PBO210117061Latihan13Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan13.relational;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini berisi program  untuk menampilaka operasi
+ *                        bilangan dengan operator relasional
+ */
+public class PBO210117061Latihan13Relational {
+    
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
